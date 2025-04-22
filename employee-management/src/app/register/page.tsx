19ce6e32b0f206afc94b61729cf0d464cabb1fed
LKappagoda00 <lahirupandukakappagoda@gmail.com>
2025-04-22 15:32:58 +0530

    Update page.tsx

--- employee-management/src/app/register/page.tsx
+++ employee-management/src/app/register/page.tsx
@@ -119,7 +119,7 @@ const Register = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-start pt-10 px-4">
       <AdminHeader />
-      <div className="bg-white p-8 mt-6 rounded-lg shadow-lg w-full max-w-3xl">
+      <div className="bg-white p-8 mt-6 rounded-lg shadow-lg w-full max-w-3xl text-black">
         <h2 className="text-3xl font-bold text-center text-blue-600 mb-6">
           Register New Employee
         </h2>
